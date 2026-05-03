d47bc5939b8c7ad5ebabf4de711a9a6fb5ffc5e6
bharat-apps <40429093+iamdecemberborn@users.noreply.github.com>
2026-05-04 00:57:40 +0530

    remive blow key
    remove blow and added hard code key

--- main.tf
+++ main.tf
@@ -2,32 +2,30 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.31"
+      version = "~> 6.0"
     }
   }
 }
 
+# The provider block must have region and keys
 provider "aws" {
-  region = "us-west-2"
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  default     = "awsbucketcreatedviaterrafrom"
+  region     = "us-west-2"
+  access_key = "AKIAZDZTBWAIN4MRKBEV"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucket_name
+  bucket = "awsbucketcreatedviaterrafrom"
 }
 
 resource "aws_s3_object" "sample_csv" {
   bucket = aws_s3_bucket.mybucket.id
-  key    = "sample.blew"
-  source = "sample.blew"
+  key    = "sample.csv"
+  source = "sample.csv"
 }
 
 resource "aws_s3_object" "products_csv" {
   bucket = aws_s3_bucket.mybucket.id
-  key    = "products.blew"
-  source = "products.blew"
+  key    = "products.csv"
+  source = "products.csv"
 }
